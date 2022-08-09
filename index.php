87e6df29fcdf35e96824d89cf21ac8c4b80cfa3f
fokrule <fokrule.hasan@gmail.com>
2022-08-08 21:51:38 -0400

    reverse pyramyd

--- index.php
+++ index.php
@@ -1,28 +1,39 @@
 <?php
 
 function loop($maximumStar) {
-    echo "1. Right triangle:</br>";
+    echo "1. Half pyramid:</br>";
     for( $i=1; $i<=$maximumStar; $i++ ){
+        $printingData = 1;
         for ( $j=1; $j<=$i; $j++ ){
-            echo "*";
+            echo $printingData . " ";
+            $printingData++;
         }
         echo "<br/>";
     }
-    echo "2. Pyramid:</br>";
+    echo "<br/> 2. Inverted half pyramid:</br>";
     $n = $maximumStar;
-    for ($i = 1; $i <= $n; $i++) {
-        for ($j = 1; $j <= $n; $j++) {
-            if($j<=($n-$i)){
-               echo "&nbsp";
-                  
-            }else {
-                echo "*";
-            }     
+    for ( $i=0;$i<=$n; $i++ ) {
+        $printingData = "A";
+        for ( $j = $n; $j >= $i; $j-- ) {
+            echo $printingData . " ";
+            $printingData++;
         }
-        
         echo "</br>";
-    } 
+    }
+    
+    echo "<br/> 3. Reverse Pyramid:</br>";
+    $n = $maximumStar;
+    for ( $i=$n;$i>=0; $i-- ) {
+        $printingData = "*";
+        for ($j=1;$j<=($n-$i);$j++) {
+            echo "&nbsp";
+        }
+        for ( $j = 1; $j <= (($i*2)-1); $j++ ) {
+            echo $printingData;
+        }
+        echo "</br>";
+    }
 }
-$data = 5;
+$data = 4;
 loop($data);
 ?>
